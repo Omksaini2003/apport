11ed16511d3f34e30752c909ba07a553c42afcfc
Skyfall003 <surajshit2003@gmail.com>
2024-10-10 11:41:24 +0530

    card and ticketlist

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Card from './components/Card/Card';
+import TicketList from './components/Card/TicketList';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <Card /> */}
+      <TicketList />
     </div>
   );
 }
---
+++ src/components/Card/TicketCard.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "./Card.css";
+import toDo from "../../assets/To-do.svg";
+import backlog from "../../assets/Backlog.svg";
+import inPro from "../../assets/in-progress.svg";
+
+const TicketCard = ({ ticket, user }) => {
+  const getStatusImage = (status) => {
+    switch (status) {
+      case "Todo":
+        return toDo;
+      case "In progress":
+        return backlog;
+      case "Backlog":
+        return inPro;
+      default:
+        return null; // Return null or a default image if no match
+    }
+  };
+
+  return (
+    <div className="card">
+      <div className="card-header">
+        <span className="card-id">{ticket.id}</span>
+        <div className="card-avatar">
+          <img
+            src={`https://api.example.com/users/${ticket.userId}/avatar`} // Placeholder URL
+            alt="Avatar"
+            className="avatar-img"
+          />
+        </div>
+      </div>
+      <h2 className="card-title">{ticket.title}</h2>
+      <div className="card-tags">
+        <div className="tag icon-tag">
+        <img
+            src={getStatusImage(ticket.status)}
+            alt={ticket.status}
+            className="status-img"
+        />
+        </div>
+        <div className="tag text-tag">{ticket.tag[0]}</div>
+      </div>
+      {/* <div className="card-status">
+        <p>Status: {ticket.status}</p>
+        <p>Priority: {ticket.priority}</p>
+        <p>Assigned to: {user?.name || 'Unknown'}</p>
+      </div> */}
+    </div>
+  );
+};
+
+export default TicketCard;
